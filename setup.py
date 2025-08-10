b131733696cc5b6a94ee625b2fa845f5f49cfb61
T1mohtml <timon.black.gamig@gmail.com>
2025-08-10 17:41:43 +0200

    Add files lol

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+APP = ['script.py']
+OPTIONS = {
+    'argv_emulation': True,
+    'packages': ['tkinter'],
+    'sign': None,  # Disable code signing
+}
+
+setup(
+    app=APP,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
